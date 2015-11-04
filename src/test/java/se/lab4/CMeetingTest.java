3099ca77676e435ef44a0ac3e823ef6174a2a24d
frances921 <taoa@uwindsor.ca>
2015-11-04 15:22:29 -0500

    CMeetingTest
    add some test case

--- src/test/java/se/lab4/CMeetingTest.java
+++ src/test/java/se/lab4/CMeetingTest.java
@@ -32,7 +32,7 @@ public class CMeetingTest
         System.out.println("tearDown");
         System.out.println(meeting);
     }
-
+   /*
     @Test
     public void testCompareTo()
     {
@@ -42,5 +42,42 @@ public class CMeetingTest
         int result = meeting.compareTo(other);
         assertEquals(expResult, result);
     }
+   */
+    // Regular case
+ 	@Test
+ 	public void testRegularMeeting() {	
+ 		
+ 		CMeeting meeting2 = new CMeeting(12, 0, 12, 30, TimeZone.Toronto);
+ 		
+ 	}
+ 	
+ 	// Boundary value test: One hour meeting should pass
+ 	@Test
+ 	public void testOneHourMeeting() {
+ 		CMeeting meeting3 = new CMeeting(12, 0, 13, 0, TimeZone.Toronto);
+ 	}
 
-}
+	// Boundary value test: throws exception when start time is equal to end time
+ 	@Test(expected = IllegalArgumentException.class)
+ 	public void endTimeShouldNotEqualToStartTime() {
+ 		CMeeting meeting4 = new CMeeting(12, 0, 12, 0, TimeZone.Toronto);
+ 	}
+ 	
+ 	// Throws exception when end time is before start time
+ 	@Test(expected = IllegalArgumentException.class)
+ 	public void endTimeShouldBeAfterStartTime() {
+ 		
+ 		CMeeting meeting5 = new CMeeting(12, 0, 11, 30, TimeZone.Toronto);
+ 		
+ 	}
+ 	
+ 	// Throws exception when meeting is more than 1 hour
+ 	@Test(expected = IllegalArgumentException.class)
+ 	public void meetingShouldNotBeMoreThanOneHour() {
+ 		CMeeting meeting6 = new CMeeting(12, 0, 13, 30, TimeZone.Toronto);
+ 	}
+ 	
+
+ }
+    
+    
